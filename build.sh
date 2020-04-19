7c398ea8b90082ddb43e4f64b87835aab67803c6
matheanswer <mathias.heyraud@gmail.com>
2020-04-20 00:23:11 +0200

    cleaning things up

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ if [ -f config ]; then
 	source config
 fi
 
-RELEASE="buster"
-
+RELEASE="${RELEASE:-"buster"}"
 BOOTSTRAP_DIR="${BOOTSTRAP_DIR:-"bootstrap"}"
 ROOTFS_DIR="${ROOTFS_DIR:-"rootfs"}"
 
@@ -17,16 +16,21 @@ PASSWORD="${PASSWORD:-"raspberry"}"
 HOSTNAME="${HOSTNAME:-"raspberrypi"}"
 LOCALE="${LOCALE:-"en_US.UTF-8"}"
 TIMEZONE="${TIMEZONE:-"UTC"}"
-USER_SHELL="${USER_SHELL:-"bash"}"
+
+if $(( SWAPSIZE < 100 )); then
+	SWAPSIZE="100"
+fi
+
+PACKAGES+=" raspberrypi-bootloader raspberrypi-kernel libraspberrypi-bin libraspberrypi0 dosfstools dphys-swapfile fake-hwclock locales whiptail"
 
 # Bootstrap
-if [ ! "${SKIP_BOOTSTRAP}" == "1" ]; then
+if [ "${SKIP_BOOTSTRAP}" == "1" ]; then
+	echo -e "${STYLE}skipping bootstrap stage${CLEAR}"
+else
 	echo -e "${STYLE}bootstrap${CLEAR}"
 	rm -rf "${BOOTSTRAP_DIR}"
 	mkdir "${BOOTSTRAP_DIR}"
-	qemu-debootstrap --arch armhf --components main,contrib,non-free --keyring files/raspberrypi.gpg ${RELEASE} ${BOOTSTRAP_DIR} http://raspbian.raspberrypi.org/raspbian
-else
-	echo -e "${STYLE}skipping bootstrap stage${CLEAR}"
+	qemu-debootstrap --arch armhf --components main,contrib,non-free --keyring files/raspbian.gpg ${RELEASE} ${BOOTSTRAP_DIR} http://raspbian.raspberrypi.org/raspbian
 fi
 
 # Rsync rootfs
@@ -35,31 +39,20 @@ rm -rf "${ROOTFS_DIR}"
 rsync -aHAXx --stats -h "${BOOTSTRAP_DIR}/" "${ROOTFS_DIR}/"
 
 # Package manager
-echo -e "${STYLE}apt configuration${CLEAR}"
-install -v -m 644 files/50raspberrypi "${ROOTFS_DIR}/etc/apt/apt.conf.d/"
-install -v -m 644 files/90recommends "${ROOTFS_DIR}/etc/apt/apt.conf.d/"
-chroot ${ROOTFS_DIR} apt-key add < files/raspberrypi.gpg.key
-chroot ${ROOTFS_DIR} << EOF
-echo "deb http://raspbian.raspberrypi.org/raspbian ${RELEASE} main contrib non-free rpi" > /etc/apt/sources.list
-echo "deb http://archive.raspberrypi.org/debian ${RELEASE} main" > /etc/apt/sources.list.d/raspberrypi.list
-apt-get update
-EOF
-
-# Locales
-# always generating en_US.UTF-8 seems to prevent warnings
-echo -e "${STYLE}locales${CLEAR}"
-chroot ${ROOTFS_DIR} << EOF
-debconf-set-selections << SELEOF
-locales locales/locales_to_be_generated multiselect ${LOCALE} UTF-8, en_US.UTF-8 UTF-8
-locales locales/default_environment_locale select ${LOCALE}
-SELEOF
-DEBIAN_FRONTEND=noninteractive apt-get install -y locales
-EOF
-
-# Packages 
-echo -e "${STYLE}packages${CLEAR}"
+echo -e "${STYLE}apt config${CLEAR}"
+install -v -m 644 files/sources.list "${ROOTFS_DIR}/etc/apt/sources.list"
+sed -i "s/RELEASE/${RELEASE}/" "${ROOTFS_DIR}/etc/apt/sources.list"
+install -v -m 644 files/99pdiffs "${ROOTFS_DIR}/etc/apt/apt.conf.d/"
+install -v -m 644 files/99recommends "${ROOTFS_DIR}/etc/apt/apt.conf.d/"
+chroot ${ROOTFS_DIR} apt-key add < files/raspbian.gpg.key
 chroot ${ROOTFS_DIR} << EOF
 export DEBIAN_FRONTEND=noninteractive
+export LC_ALL=C
+debconf-set-selections << SELEOF
+locales locales/locales_to_be_generated multiselect ${LOCALE} UTF-8
+locales locales/default_environment_locale select ${LOCALE}
+SELEOF
+apt-get update
 apt-get dist-upgrade -y
 apt-get install -y ${PACKAGES}
 apt-get clean
@@ -74,23 +67,27 @@ echo "${USERNAME}:${PASSWORD}" | chpasswd
 echo "root:root" | chpasswd
 usermod --pass='*' root
 EOF
-if [ "${USER_SHELL}" == "zsh" ]; then
-	install -v -m 644 -g 1000 -o 1000 files/grml-zshrc "${ROOTFS_DIR}/home/${USERNAME}/.zshrc"
-fi
+install -v -m 644 -g 1000 -o 1000 files/zshrc "${ROOTFS_DIR}/home/${USERNAME}/.zshrc"
+install -v -m 644 -g 1000 -o 1000 files/zshrc.local "${ROOTFS_DIR}/home/${USERNAME}/.zshrc.local"
+sed -i "s/LOCALE/${LOCALE}/" "${ROOTFS_DIR}/home/${USERNAME}/.zshrc.local"
+
+echo -e "${STYLE}configuration${CLEAR}"
 
 # Networking
-echo -e "${STYLE}networking${CLEAR}"
 echo "${HOSTNAME}" > "${ROOTFS_DIR}/etc/hostname"
 echo "127.0.1.1	${HOSTNAME}" >> "${ROOTFS_DIR}/etc/hosts"
 ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
 
 # Time Zone
-echo -e "${STYLE}time zone${CLEAR}"
 chroot ${ROOTFS_DIR} << EOF
 echo "${TIMEZONE}" > "/etc/timezone"
 rm "/etc/localtime"
 dpkg-reconfigure -f noninteractive tzdata
 EOF
+
+# Swap
+install -v -m 644 files/99-swappiness.conf "${ROOTFS_DIR}/etc/sysctl.d/"
+echo "CONF_SWAPSIZE=${SWAPSIZE}" >> "${ROOTFS_DIR}/etc/dphys-swapfile"
 
 # Boot files
 echo -e "${STYLE}boot files${CLEAR}"
